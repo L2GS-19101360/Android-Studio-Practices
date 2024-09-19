660ed289ec660d52dd4a8d91dfa6ac32f469298d
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-09-20 00:41:26 +0800

    Updates

---
+++ ListViewApplication/app/src/main/java/com/example/listviewapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.listviewapplication;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    String [] options = {"Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6", "Option 7", "Option 8"};
+
+    ListView listViewOption;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        listViewOption = findViewById(R.id.optionlistview);
+
+        ArrayAdapter<String> optionAdapter = new ArrayAdapter<>(this, R.layout.listview_item, R.id.listviewitem, options);
+
+        listViewOption.setAdapter(optionAdapter);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        listViewOption.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(getApplicationContext(), options[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
