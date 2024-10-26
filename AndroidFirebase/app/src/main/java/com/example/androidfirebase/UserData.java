714d7baa962d144968414367d84ebcce05e3c3c2
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-10-26 15:02:47 +0800

    Update

---
+++ AndroidFirebase/app/src/main/java/com/example/androidfirebase/UserData.java
@@ -0,0 +1,36 @@
+package com.example.androidfirebase;
+
+public class UserData {
+
+    private String key, username, password;
+
+    public UserData(String key, String username, String password) {
+        this.key = key;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
