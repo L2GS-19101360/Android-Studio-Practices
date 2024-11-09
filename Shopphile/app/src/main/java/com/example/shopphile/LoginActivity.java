a976f45b5314b7ff10eff6f111ec7a63e99e23c3
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-11-09 11:51:36 +0800

    Login and Register Activity Components

--- Shopphile/app/src/main/java/com/example/shopphile/LoginActivity.java
+++ Shopphile/app/src/main/java/com/example/shopphile/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.shopphile;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView toRegisterActivity;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        toRegisterActivity = findViewById(R.id.toregisteractivity);
+        toRegisterActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- Shopphile/app/src/main/java/com/example/shopphile/RegisterActivity.java
+++ Shopphile/app/src/main/java/com/example/shopphile/RegisterActivity.java
@@ -1,6 +1,10 @@
 package com.example.shopphile;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView toLoginActivity;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+
+        toLoginActivity = findViewById(R.id.tologinactivity);
+        toLoginActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
