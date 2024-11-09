d93670b482d583295325466b9c7cfa1f144835bb
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-11-09 16:23:30 +0800

    Update LoginActivity.java

--- Shopphile/app/src/main/java/com/example/shopphile/LoginActivity.java
+++ Shopphile/app/src/main/java/com/example/shopphile/LoginActivity.java
@@ -3,16 +3,30 @@ package com.example.shopphile;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +34,8 @@ public class LoginActivity extends AppCompatActivity {
     Button loginButton;
 
     TextView toRegisterActivity;
+
+    private FirebaseAuth fbAuth;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -31,11 +47,13 @@ public class LoginActivity extends AppCompatActivity {
         userEmailInput = findViewById(R.id.useremailinput);
         userPasswordInput = findViewById(R.id.userpasswordinput);
 
+        fbAuth = FirebaseAuth.getInstance();
+
         loginButton = findViewById(R.id.loginbutton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                loginUserAccount();
             }
         });
 
@@ -48,5 +66,48 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void loginUserAccount() {
+        if (userEmailInput.getText().toString().trim().isEmpty()) {
+            userEmailInput.setError("Email Required!");
+            return;
+        } else if (!isValidEmailPattern(userEmailInput.getText().toString())) {
+            userEmailInput.setError("Invalid Email Address!");
+            return;
+        }
+        if (userPasswordInput.getText().toString().trim().isEmpty()) {
+            userPasswordInput.setError("Password Required!");
+            return;
+        }
+
+        String userEmail = userEmailInput.getText().toString().trim();
+        String userPassword = userPasswordInput.getText().toString().trim();
+
+        fbAuth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "User login failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    public boolean isValidEmailPattern(final String email) {
+        Pattern pattern;
+        Matcher matcher;
+
+        final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+
+
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+
+        return matcher.matches();
     }
 }
