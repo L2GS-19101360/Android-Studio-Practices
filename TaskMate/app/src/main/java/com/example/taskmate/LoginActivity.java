77fbf1b055378e4f3cbc0fdef5d628e82a887409
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-11-20 11:16:40 +0800

    Login and Register Frontend

---
+++ TaskMate/app/src/main/java/com/example/taskmate/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.taskmate;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText userEmailInput, userPasswordInput;
+    Button loginButton;
+    TextView toRegisterPage;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        userEmailInput = findViewById(R.id.useremailinput);
+        userPasswordInput = findViewById(R.id.userpasswordinput);
+
+        loginButton = findViewById(R.id.loginbutton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        toRegisterPage = findViewById(R.id.toregisterpage);
+        toRegisterPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
