3a4d51580e67a00e1cb92e031c3ba187d998ab24
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-11-25 23:46:37 +0800

    New App

---
+++ ChatMates/app/src/main/java/com/example/chatmates/activities/SignUpActivity.java
@@ -0,0 +1,32 @@
+package com.example.chatmates.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chatmates.R;
+import com.example.chatmates.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textSignInAccount.setOnClickListener(view ->
+                startActivity(new Intent(getApplicationContext(), SignInActivity.class)));
+    }
+}
