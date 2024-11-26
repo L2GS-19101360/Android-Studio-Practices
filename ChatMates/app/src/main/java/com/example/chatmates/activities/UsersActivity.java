2950cd2ea4316973052738454c930e35cae736d5
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-11-26 11:44:00 +0800

    Update

---
+++ ChatMates/app/src/main/java/com/example/chatmates/activities/UsersActivity.java
@@ -0,0 +1,22 @@
+package com.example.chatmates.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chatmates.R;
+
+public class UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_users);
+
+    }
+}
